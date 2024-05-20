56b6cc7201e8b9a78a5f5a7692134612d0827293
Mali <kul.mehmetali@yahoo.com>
2024-05-20 21:08:36 +0300

    gitignore

--- routes/userRouter.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from 'express';
-import {matchController} from '../controller/matchController'
-const router = express.Router();
-
-router.post('/match', matchController);
-
-export default router;
